a817c58c068f147653aeee5efefe1d75e2708e79
Leonard Hecker <leonard@hecker.io>
2015-09-24 22:57:12 +0200

    bootstrapping

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 'use strict';
 
 const Connection = require('./lib/Connection');
+const pg = require('pg');
 
 var Promise = require('bluebird');
 
 
-Promise.using(Connection.connect({
-	user    : 'postgres',
-	password: 'qyce-zhju',
-	database: 'eucstats',
-}), (conn) => {
-	return conn.query('SELECT now()')
-		.then(console.log.bind(console));
+/*console.time('pg');
+
+pg.connect('postgres://postgres:qyce-zhju@localhost/eucstats', function (err, conn, done) {
+	conn.query("SELECT * FROM events_sessions", function (err, result) {
+		console.timeEnd('pg');
+		done();
+	});
 });
+
+
+setTimeout(() => {*/
+	console.time('postgres');
+
+	Promise.using(Connection.connect({
+		user    : 'postgres',
+		password: 'qyce-zhju',
+		database: 'eucstats',
+	}), (conn) => {
+		return conn.query("SELECT * FROM events_sessions")
+			//.then(results => console.log(require('util').inspect(results, {depth:null, colors:true})));
+			.then((results) => {
+				console.timeEnd('postgres');
+			});
+	});
+//}, 2000);
